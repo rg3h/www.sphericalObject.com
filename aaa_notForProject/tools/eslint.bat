ab01c00f1765e2a56bbafa5f80d65ea7eaff6d18
rg3h <rich.gossweiler@gmail.com>
2026-01-31 17:00:03 -0500

    remove aaa_notForProject

--- aaa_notForProject/tools/eslint.bat
+++
@@ -1,7 +0,0 @@
-:: @fileoverview eslint.bat runs npm eslint
-:: usage: eslint.bat someFile.js
-@echo off
-setlocal
-cd ../src
-npx eslint %*
-endlocal
--- aaa_notForProject/tools/runServer.bat
+++
@@ -1,6 +0,0 @@
-:: @fileoverview runServer.bat runs a local node web server
-@echo off
-clear
-setlocal
-start node ./server/simpleServer.js --webroot "../../src/"
-endlocal
